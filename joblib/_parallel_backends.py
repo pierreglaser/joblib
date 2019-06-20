222fdd99eceef7563eab30467d01c2109d29e778
Pierre Glaser <pierreglaser@msn.com>
2019-06-20 19:07:22 +0200

    PERF decrease batch size quickly

--- joblib/_parallel_backends.py
+++ joblib/_parallel_backends.py
@@ -292,7 +292,14 @@ class AutoBatchingMixin(object):
             # while a couple of CPUs a left processing a few long running
             # batches. Better reduce the batch size a bit to limit the
             # likelihood of scheduling such stragglers.
-            batch_size = old_batch_size // 2
+
+            # decrease the batch size quickly to limit potential starving
+            ideal_batch_size = int(
+                old_batch_size * self.MIN_IDEAL_BATCH_DURATION / batch_duration
+            )
+            # Multiply by two to limit oscilations between min and max.
+            batch_size = max(2 * ideal_batch_size, 1)
+
             self._effective_batch_size = batch_size
             if self.parallel.verbose >= 10:
                 self.parallel._print(
